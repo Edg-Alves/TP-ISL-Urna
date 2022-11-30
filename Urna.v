20b3e72f01cb5804f6822d58043213ae6f65ceb6
Edgar Alves <91341310+Edg-Alves@users.noreply.github.com>
2022-11-30 20:35:20 -0300

    Update Urna.v

--- Urna.v
+++ Urna.v
@@ -1,7 +1,8 @@
 module Urna #(
 parameter inicio = 2'b00,
 parameter digito1 = 2'b01,
-parameter digito2 = 2'b10)
+parameter digito2 = 2'b10,
+parameter finish_vote = 2'b11)
 (
     input clk,							
     input digit,				
@@ -44,7 +45,7 @@ always @(negedge finish or posedge valid)
 				
 			digito1: if (finish == 1'b1)									
 						begin
-							proximo_estado <= finish;								
+							proximo_estado <= finish_vote;								
 						end
 					else if (digit == 4'b0001 && valid == 1'b1)
 						begin
@@ -60,3 +61,38 @@ always @(negedge finish or posedge valid)
 						begin
 							proximo_estado <= digito2;
 						end
+			digito2: if (finish == 1'b1)
+						begin
+							proximo_estado <= finish_vote;
+						end
+					else if (digit == 4'b0011 && valid == 1'b1)
+						begin
+							contadorC1 <= contadorC1 + 1'b1;
+							VoteStatus <= 1'b1;
+							proximo_estado <= digito1;
+						end
+					else if (digit == 4'b0010 && valid == 1'b1)
+						begin
+							contadorC2 <= contadorC2 + 1'b1;
+							VoteStatus <= 1'b1;
+							proximo_estado <= digito1;
+						end
+					else if (digit == 4'b0010 && valid == 1'b1 && swap == 1'b1)
+						begin
+							contadorC1 <= contadorC1 + 1'b1;
+							VoteStatus <= 1'b1;
+							proximo_estado <= digito1;
+						end
+					else if (digit == 4'b0011 && valid == 1'b1 && swap == 1'b1)
+						begin
+							contadorC2 <= contadorC2 + 1'b1;
+							VoteStatus <= 1'b1;
+							proximo_estado <= digito1;
+						end
+					else 
+						begin
+							contadorNulL <= contadorNulL + 1'b1;
+							VoteStatus <= 1'b0;
+							proximo_estado <= digito1;
+						end
+			
